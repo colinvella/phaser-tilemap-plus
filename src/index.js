ac7e0ff73bb40e76a934a9990e09d51573491ec7
Colin <colinvella@gmail.com>
2017-10-26 21:11:48 +0200

    Removed imports for Phaser to avoid repeated modules, restructured index.js and linked to test game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import "./tilemap-plus/TilemapPlus";
+import TilemapPlus from "./tilemap-plus/TilemapPlus";
+
+Phaser.Plugin.TilemapPlus = function (game, parent) {
+	Phaser.Plugin.call(this, game, parent);
+    
+    Phaser.Loader.prototype.tilemapPlus = function(key, url, data, format) {
+        this.tilemap(key, url, data, format);
+        this.json(jsonKey(key), url);
+    };
+    
+    Phaser.GameObjectFactory.prototype.tilemapPlus = function(key, tileWidth, tileHeight, width, height) {
+        const tilemap = this.tilemap(key, tileWidth, tileHeight, width, height);
+        const tilemapJson = this.game.cache.getJSON(jsonKey(key)); 
+        tilemap.plus = new TilemapPlus(tilemapJson, this.game.time, tilemap);
+        return tilemap;
+    };
+
+    function jsonKey(key) {
+        return key + "-TilemapPlus";
+    }
+};
